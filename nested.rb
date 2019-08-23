a0eca0e4b1a6941c5eab5549512edb8e644d0691
Catherine Clause <cnclause123@gmail.com>
2019-08-23 21:29:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,8 +86,9 @@ def adding_matz
  
  puts programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
-   :languages => ["LISP", "C"]]
+   :languages => ["LISP", "C"]
     
+    return programmer_hash
 end
 
 def changing_alan

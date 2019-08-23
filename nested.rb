65b01281774dd8727f7773c912582ed811062df3
Catherine Clause <cnclause123@gmail.com>
2019-08-23 21:46:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,7 @@ def adding_to_dennis
      } 
      
      programmer_hash[:dennis_ritchie][:languages] = 
-      ["Assembly", "C"] 
+      ["C", "Assembly"] 
       
       return programmer_hash
      
